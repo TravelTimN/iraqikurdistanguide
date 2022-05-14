18f43bc21f6d340c0cc1100190d8016db16144cc
Tim Nelson <traveltimn@gmail.com>
2022-05-14 23:07:15 +0100

    Add script for dismissing alerts automatically

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,29 @@
 /* jshint esversion: 11 */
+
+// update copyright year
+document.getElementById("year").innerText = new Date().getFullYear();
+
+// auto-hide alerts
+const alerts = document.querySelectorAll("aside.alert");
+let overlay = document.getElementById("overlay");
+if (alerts.length > 0) {
+    // only if any alerts found on DOM
+    overlay?.classList.remove("hide");
+    for (let i = 0; i < alerts.length; i++) {
+        setTimeout(() => {
+            // start after 2500ms
+            setTimeout(() => {
+                // slight delay between each alert
+                alerts[i].classList.add("alert-slide");
+                setTimeout(() => {
+                    // remove from DOM entirely
+                    alerts[i].style.display = "none";
+                }, 1000);
+            }, i * 100);
+            // remove the overlay background
+            setTimeout(() => {
+                overlay?.classList.add("hide");
+            }, 500);
+        }, 2500);
+    }
+}
