9012eeaab3bb5e4c7a4e68012f792938eebcc266
Tim Nelson <traveltimn@gmail.com>
2022-05-14 12:32:00 +0100

    Add error handling urls

--- main/urls.py
+++ main/urls.py
@@ -2,8 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404, handler500
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = "main.views.handler404"
+handler500 = "main.views.handler500"
