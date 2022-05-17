68de3273b920ea9d5336bab70245e149be5bf0a7
Tim Nelson <traveltimn@gmail.com>
2022-05-17 22:06:53 +0100

    Update destination urls with specific IDs

--- destinations/urls.py
+++ destinations/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.destinations, name="destinations"),
+    path("<int:id>/", views.destination, name="destination"),
     path("add/", views.add_destination, name="add_destination"),
-    path("update/", views.update_destination, name="update_destination"),
+    path("update/<int:id>", views.update_destination, name="update_destination"),
 ]
