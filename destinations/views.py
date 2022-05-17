8db7ba422581d0beec4c75ca4c9ba5f6c3526d3c
Tim Nelson <traveltimn@gmail.com>
2022-05-17 22:07:17 +0100

    Update destinations views for add/update/view

--- destinations/views.py
+++ destinations/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
-from .models import Destination
+from django.contrib import messages
+from .models import Destination, Site
 
 
 def destinations(request):
@@ -15,11 +16,40 @@ def destinations(request):
     return render(request, template, context)
 
 
+def destination(request, id):
+    """ A view to return the destinations page """
+    map_url = settings.MAP_URL
+    destination = get_object_or_404(Destination, id=id)
+    sites = Site.objects.filter(destination=destination)
+    template = "destinations/destination.html"
+    context = {
+        "map_url": map_url,
+        "destination": destination,
+        "sites": sites,
+    }
+    return render(request, template, context)
+
+
 def add_destination(request):
     """ A view to add a single destination """
-    return render(request, "destinations/add_destination.html")
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    template = "destinations/add_destination.html"
+    context = {}
+    return render(request, template, context)
 
 
-def update_destination(request):
+def update_destination(request, id):
     """ A view to update a specific destination """
-    return render(request, "destinations/update_destination.html")
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    destination = get_object_or_404(Destination, id=id)
+    template = "destinations/update_destination.html"
+    context = {
+        "destination": destination,
+    }
+    return render(request, template, context)
