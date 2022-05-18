fa8f3ecc0ba86184d8c537fcb8d02a8763f505bd
Tim Nelson <traveltimn@gmail.com>
2022-05-18 20:09:48 +0100

    Update Destination urls/views, and add new Sites functionality

--- destinations/views.py
+++ destinations/views.py
@@ -16,20 +16,6 @@ def destinations(request):
     return render(request, template, context)
 
 
-def destination(request, id):
-    """ A view to return the destinations page """
-    map_url = settings.MAP_URL
-    destination = get_object_or_404(Destination, id=id)
-    sites = Site.objects.filter(destination=destination)
-    template = "destinations/destination.html"
-    context = {
-        "map_url": map_url,
-        "destination": destination,
-        "sites": sites,
-    }
-    return render(request, template, context)
-
-
 def add_destination(request):
     """ A view to add a single destination """
     if not request.user.is_superuser:
@@ -38,6 +24,20 @@ def add_destination(request):
         return redirect(reverse("destinations"))
     template = "destinations/add_destination.html"
     context = {}
+    return render(request, template, context)
+
+
+def view_destination(request, id):
+    """ A view to return the destination-specific page """
+    map_url = settings.MAP_URL
+    destination = get_object_or_404(Destination, id=id)
+    sites = Site.objects.filter(destination=destination)
+    template = "destinations/view_destination.html"
+    context = {
+        "map_url": map_url,
+        "destination": destination,
+        "sites": sites,
+    }
     return render(request, template, context)
 
 
@@ -51,5 +51,49 @@ def update_destination(request, id):
     template = "destinations/update_destination.html"
     context = {
         "destination": destination,
+    }
+    return render(request, template, context)
+
+
+def add_site(request, id):
+    """ A view to add a single site/POI """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    destination = get_object_or_404(Destination, id=id)
+    template = "destinations/add_site.html"
+    context = {
+        "destination": destination,
+    }
+    return render(request, template, context)
+
+
+def view_site(request, d_id, s_id):
+    """ A view to return the site-specific page """
+    map_url = settings.MAP_URL
+    destination = get_object_or_404(Destination, id=d_id)
+    site = get_object_or_404(Site, id=s_id)
+    template = "destinations/view_site.html"
+    context = {
+        "map_url": map_url,
+        "destination": destination,
+        "site": site,
+    }
+    return render(request, template, context)
+
+
+def update_site(request, d_id, s_id):
+    """ A view to update a specific site """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    destination = get_object_or_404(Destination, id=d_id)
+    site = get_object_or_404(Site, id=s_id)
+    template = "destinations/update_site.html"
+    context = {
+        "destination": destination,
+        "site": site,
     }
     return render(request, template, context)
