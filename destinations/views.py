f16e7b82470afee4bfd35039981c83e5cd039f8d
Tim Nelson <traveltimn@gmail.com>
2022-05-21 13:29:26 +0100

    Update CRUD functionality for destinations/sites

--- destinations/views.py
+++ destinations/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.conf import settings
 from django.contrib import messages
+from django.http import HttpResponseRedirect
 from .models import Destination, Site
-from .forms import DestinationForm
+from .forms import DestinationForm, SiteForm
 
 
 MAP_URL = settings.MAP_URL
@@ -28,11 +29,11 @@ def add_destination(request):
     destination_form = DestinationForm(request.POST or None)
     if request.method == "POST":
         if destination_form.is_valid():
+            next = request.POST.get("next", "/")
             new_destination = destination_form.save()
-            messages.success(request, "Destination Added!")
-            return redirect(view_destination, new_destination.pk)
+            messages.success(request, f"{new_destination.name} Added!")
+            return HttpResponseRedirect(next)
         messages.error(request, "Error: Please Try Again.")
-    destination_form = DestinationForm()
     template = "destinations/add_destination.html"
     context = {
         "map_url": MAP_URL,
@@ -64,9 +65,10 @@ def update_destination(request, id):
     destination_form = DestinationForm(request.POST or None, instance=destination)
     if request.method == "POST":
         if destination_form.is_valid():
-            destination_form.save()
-            messages.success(request, "Destination Updated!")
-            return redirect(view_destination, id)
+            next = request.POST.get("next", "/")
+            updated_destination = destination_form.save()
+            messages.success(request, f"{updated_destination.name} Updated!")
+            return HttpResponseRedirect(next)
         messages.error(request, "Error: Please Try Again.")
     destination_form = DestinationForm(instance=destination)
     template = "destinations/update_destination.html"
@@ -78,6 +80,18 @@ def update_destination(request, id):
     return render(request, template, context)
 
 
+def delete_destination(request, id):
+    """ A view to delete a single destination """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    destination = get_object_or_404(Destination, id=id)
+    messages.success(request, f"{destination.name} Deleted!")
+    destination.delete()
+    return redirect(reverse("destinations"))
+
+
 def add_site(request, id):
     """ A view to add a single site/POI """
     if not request.user.is_superuser:
@@ -85,9 +99,19 @@ def add_site(request, id):
         messages.error(request, "Access denied. Invalid permissions.")
         return redirect(reverse("destinations"))
     destination = get_object_or_404(Destination, id=id)
+    site_form = SiteForm(request.POST or None, initial={"destination": id})
+    if request.method == "POST":
+        if site_form.is_valid():
+            next = request.POST.get("next", "/")
+            new_site = site_form.save()
+            messages.success(request, f"{new_site.name} Added!")
+            return HttpResponseRedirect(next)
+        messages.error(request, "Error: Please Try Again.")
     template = "destinations/add_site.html"
     context = {
+        "map_url": MAP_URL,
         "destination": destination,
+        "site_form": site_form,
     }
     return render(request, template, context)
 
@@ -113,9 +137,32 @@ def update_site(request, d_id, s_id):
         return redirect(reverse("destinations"))
     destination = get_object_or_404(Destination, id=d_id)
     site = get_object_or_404(Site, id=s_id)
+    site_form = SiteForm(request.POST or None, instance=site)
+    if request.method == "POST":
+        if site_form.is_valid():
+            next = request.POST.get("next", "/")
+            updated_site = site_form.save()
+            messages.success(request, f"{updated_site.name} Updated!")
+            return HttpResponseRedirect(next)
+        messages.error(request, "Error: Please Try Again.")
+    site_form = SiteForm(instance=site)
     template = "destinations/update_site.html"
     context = {
+        "map_url": MAP_URL,
         "destination": destination,
         "site": site,
+        "site_form": site_form,
     }
     return render(request, template, context)
+
+
+def delete_site(request, d_id, s_id):
+    """ A view to delete a single site """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to all destinations
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("destinations"))
+    site = get_object_or_404(Site, id=s_id)
+    messages.success(request, f"{site.name} Deleted!")
+    site.delete()
+    return redirect(reverse("destinations"))
