b100b708ca1c0afbe05ea52f444347afd272e485
Tim Nelson <traveltimn@gmail.com>
2022-05-21 21:15:38 +0100

    Add TinyMCE form widget for FAQs

--- faqs/forms.py
+++ faqs/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from tinymce.widgets import TinyMCE
 from .models import FAQ
 
 
@@ -6,6 +7,8 @@ class FAQForm(forms.ModelForm):
     """
         Form to allow admins to manage FAQs.
     """
+    answer = forms.CharField(widget=TinyMCE(attrs={"cols": 30, "rows": 15}))
+
     class Meta:
         model = FAQ
         fields = "__all__"
