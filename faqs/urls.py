d9dc65d2d0fd92aad0e3493e8cfdf37488b42834
Tim Nelson <traveltimn@gmail.com>
2022-05-21 21:16:00 +0100

    Update FAQ urls for update/delete functionality

--- faqs/urls.py
+++ faqs/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.faqs, name="faqs"),
     path("add/", views.add_faq, name="add_faq"),
-    path("update/", views.update_faq, name="update_faq"),
+    path("update/<int:id>", views.update_faq, name="update_faq"),
+    path("delete/<int:id>", views.delete_faq, name="delete_faq"),
 ]
