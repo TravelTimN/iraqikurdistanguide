09e06c4eb6bef05ef7ad321e52973382bfaebbc2
Tim Nelson <traveltimn@gmail.com>
2022-05-22 23:33:40 +0100

    Add CRUD functionality for gallery and photos

--- gallery/views.py
+++ gallery/views.py
@@ -1,16 +1,78 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from .models import Photo
+from .forms import PhotoForm
 
 
 def gallery(request):
     """ A view to return the gallery page """
-    return render(request, "gallery/gallery.html")
+    if request.user.is_superuser:
+        photos = Photo.objects.all().order_by("sight__destination")
+    else:
+        photos = Photo.objects.filter(is_visible=True).order_by("sight__destination")
+    template = "gallery/gallery.html"
+    context = {
+        "photos": photos,
+    }
+    return render(request, template, context)
 
 
-def add_image(request):
-    """ A view to add a single image """
-    return render(request, "gallery/add_image.html")
+@login_required
+def add_photo(request):
+    """ A view to add a single photo """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to the gallery
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("gallery"))
+    photo_form = PhotoForm(request.POST or None, request.FILES or None)
+    if request.method == "POST":
+        if photo_form.is_valid():
+            next = request.POST.get("next", "/")
+            photo_form.save()
+            messages.success(request, "Photo Added!")
+            return HttpResponseRedirect(next)
+        messages.error(request, "Error: Please Try Again.")
+    template = "gallery/add_photo.html"
+    context = {
+        "photo_form": photo_form,
+    }
+    return render(request, template, context)
 
 
-def update_image(request):
-    """ A view to update a specific image """
-    return render(request, "gallery/update_image.html")
+@login_required
+def update_photo(request, id):
+    """ A view to update a single photo """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to the gallery
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("gallery"))
+    photo = get_object_or_404(Photo, id=id)
+    photo_form = PhotoForm(request.POST or None, request.FILES or None, instance=photo)
+    if request.method == "POST":
+        if photo_form.is_valid():
+            next = request.POST.get("next", "/")
+            photo_form.save()
+            messages.success(request, "Photo Updated!")
+            return HttpResponseRedirect(next)
+        messages.error(request, "Error: Please Try Again.")
+    template = "gallery/update_photo.html"
+    context = {
+        "photo": photo,
+        "photo_form": photo_form,
+    }
+    return render(request, template, context)
+
+
+@login_required
+def delete_photo(request, id):
+    """ A view to delete a specific photo """
+    if not request.user.is_superuser:
+        # user is not superuser; take them to the gallery
+        messages.error(request, "Access denied. Invalid permissions.")
+        return redirect(reverse("gallery"))
+    photo = get_object_or_404(Photo, id=id)
+    photo.delete()
+    messages.success(request, "Photo Deleted!")
+    return redirect(reverse("gallery"))
