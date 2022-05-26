38a540f87ab21dbc21262dbea5c3d28d9d007ece
Tim Nelson <traveltimn@gmail.com>
2022-05-26 22:35:42 +0100

    Generate views.py for Reviews template

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Review
 
 
 def reviews(request):
     """ A view to return the reviews page """
-    return render(request, "reviews/reviews.html")
+    if request.user.is_superuser:
+        reviews = Review.objects.all().order_by("date", "rating")
+    else:
+        reviews = Review.objects.filter(is_visible=True).order_by("date", "rating")
+    template = "reviews/reviews.html"
+    context = {
+        "reviews": reviews,
+    }
+    return render(request, template, context)
