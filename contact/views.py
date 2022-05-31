3c3f6e2bf43effa2dca8b08efa37ae69a5cd8595
10xOXR <41506849+10xOXR@users.noreply.github.com>
2022-05-31 22:15:39 +0100

    Add contact form GET/POST handling

--- contact/views.py
+++ contact/views.py
@@ -1,6 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from .models import Contact
+from .forms import ContactForm
+from django.contrib import messages
 
 
 def contact(request):
     """ A view to return the contact page """
-    return render(request, "contact/contact.html")
+    contact_form = ContactForm(request.POST or None)
+    if request.method == "POST":
+        if contact_form.is_valid:
+            contact_form.save()
+            messages.success(request, "Message sent!")
+            return redirect(reverse("contact"))
+    template = "contact/contact.html"
+    context = {
+        "contact_form": contact_form,
+    }
+    return render(request, template, context)
