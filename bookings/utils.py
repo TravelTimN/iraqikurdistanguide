179247add507ab5fd6c10c64dc983e073bc0b245
Tim Nelson <traveltimn@gmail.com>
2022-06-02 22:27:00 +0100

    Filter calendar booking by active/visible month only

--- bookings/utils.py
+++ bookings/utils.py
@@ -1,5 +1,6 @@
 import calendar
 from datetime import datetime
+from django.db.models import Q
 from .models import Booking
 
 
@@ -12,9 +13,15 @@ class BookingCalendar(calendar.HTMLCalendar):
         super(BookingCalendar, self).__init__()
 
     # filter bookings by day
-    def formatday(self, day, weekday, month, bookings):
+    def formatday(self, day, weekday, month, year, bookings):
         # add booking to every day that a guest's trip exists
-        bookings_per_day = bookings.filter(start_date__day__lte=day, end_date__day__gte=day)
+        bookings_per_day = bookings.filter(
+            Q(start_date__day__lte=day, end_date__day__gte=day) |
+            Q(start_date__day__lte=day, end_date__month__gt=month) |
+            Q(start_date__month__lt=month, end_date__day__gte=day) |
+            Q(start_date__year__lt=year, end_date__day__gte=day) |
+            Q(start_date__day__lte=day, end_date__year__gt=year)
+        )
         d = ""
         for booking in bookings_per_day:
             # truncate longer guest names
@@ -25,10 +32,10 @@ class BookingCalendar(calendar.HTMLCalendar):
             # if the day of month is "today"
             if day == datetime.now().day and month == datetime.now().month:
                 return f"""
-                <div class="col-md day p-2 border border-danger text-truncate">
-                    <p class="row align-items-center fs-4 bg-danger bg-gradient text-white">
+                <div class="col-md day p-2 bg-warning bg-opacity-25 text-truncate">
+                    <p class="row align-items-center fs-4">
                         <span class="date col-1">{day}</span>
-                        <small class="col d-md-none text-center text-white">{calendar.day_name[weekday]}</small>
+                        <small class="col d-md-none text-center">{calendar.day_name[weekday]}</small>
                         <span class="col-1"></span>
                     </p>
                     <p class="d-md-none">No Bookings</p>
@@ -74,20 +81,24 @@ class BookingCalendar(calendar.HTMLCalendar):
         """
 
     # formats a week as individual row
-    def formatweek(self, theweek, bookings, month):
+    def formatweek(self, theweek, bookings, month, year):
         week = ""
         # loop day/weekday to generate each daily booking event
         for d, weekday in theweek:
-            week += self.formatday(d, weekday, month, bookings)
+            week += self.formatday(d, weekday, month, year, bookings)
         # must have .w-100 div after each week to force new-line
         return f"{week}<div class='w-100'></div>"
 
     # filter bookings by year/month
     def formatmonth(self, withyear=True):
-        bookings = Booking.objects.filter(start_date__year=self.year, start_date__month=self.month)
+        bookings = Booking.objects.filter(
+            Q(start_date__year=self.year, start_date__month=self.month) |
+            Q(start_date__month__lt=self.month, end_date__month=self.month) |
+            Q(start_date__year__lt=self.year, end_date__month=self.month)
+        )
         # build the month/year header
         booking_calendar = f"""<header>
-            <h4 class="display-4 mb-4 text-center">{calendar.month_name[self.month]} {self.year}</h4>
+            <h4 class="display-4 text-center">{calendar.month_name[self.month]} {self.year}</h4>
             <div class="row d-none d-md-flex p-1 bg-dark text-white">
         """
         # loop days of week header
@@ -100,6 +111,6 @@ class BookingCalendar(calendar.HTMLCalendar):
         """
         # loop each week and append to the calendar
         for week in self.monthdays2calendar(self.year, self.month):
-            booking_calendar += f"{self.formatweek(week, bookings, self.month)}\n"
+            booking_calendar += f"{self.formatweek(week, bookings, self.month, self.year)}\n"
         booking_calendar += "</div>"
         return booking_calendar
