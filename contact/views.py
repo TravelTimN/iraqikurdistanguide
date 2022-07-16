2dfdb86880894dae195ddfe20ffdb09245df0521
Tim Nelson <traveltimn@gmail.com>
2022-07-16 23:24:09 +0100

    Update contact form views/forms.
    Co-authored-by: Chris Quinn <41506849+10xOXR@users.noreply.github.com>

--- contact/views.py
+++ contact/views.py
@@ -8,10 +8,13 @@ def contact(request):
     """ A view to return the contact page """
     contact_form = ContactForm(request.POST or None)
     if request.method == "POST":
-        if contact_form.is_valid:
+        if contact_form.is_valid():
             contact_form.save()
-            messages.success(request, "Message sent!")
-            return redirect(reverse("contact"))
+            messages.success(
+                request,
+                "Thank You! We'll be in touch with you soon!"
+            )
+            return redirect(reverse("home"))
     template = "contact/contact.html"
     context = {
         "contact_form": contact_form,
