c5637d60a637f023a267c3dbb992355b87051600
Tim Nelson <traveltimn@gmail.com>
2022-07-28 21:07:15 +0100

    Update FAQ forms for CRUD design

--- faqs/forms.py
+++ faqs/forms.py
@@ -12,3 +12,22 @@ class FAQForm(forms.ModelForm):
     class Meta:
         model = FAQ
         fields = "__all__"
+
+    def __init__(self, *args, **kwargs):
+        # https://stackoverflow.com/a/72025478
+        super().__init__(*args, **kwargs)
+
+        # add placeholder for floating-label functionality
+        for field in self.fields:
+            if field != "is_visible":
+                self.fields[field].widget.attrs["class"] = "form-control"
+            if field == "answer":
+                self.fields[field].widget.attrs["placeholder"] = "FAQ Answer"
+            else:
+                self.fields[field].widget.attrs["placeholder"] = field
+
+        # generate list of available FAQ categories
+        self.fields["category"].choices = [["", "Select Category"]]
+        categories = FAQ.CATEGORIES
+        for category in categories:
+            self.fields["category"].choices.append(category)
