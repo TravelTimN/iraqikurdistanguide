3930b87ed55ce15e641b00cf540cbf0381840682
Tim Nelson <traveltimn@gmail.com>
2024-05-16 20:07:50 +0200

    Create Booking for new contact requests

--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,13 @@
-import datetime
+from datetime import datetime, timedelta
 from django.shortcuts import get_object_or_404, render, redirect, reverse
 from django.conf import settings
 from django.contrib import messages
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
+from django_countries.fields import Country
 from .models import Contact
 from .forms import ContactForm
+from bookings.models import Booking
 from destinations.models import Destination
 
 
@@ -23,33 +25,61 @@ def contact(request):
             # generate list of wishlist destinations
             destinations = []
             for destination in request.POST.getlist("destinations"):
-                if isinstance(int(destination), int):
+                if destination.isdigit():
                     location = get_object_or_404(Destination, id=int(destination))
-                    destinations.append(location.name)
+                    destinations.append(location)
 
-            # form data collection
+            # create new Booking object
+            start_date = datetime.strptime(request.POST.get("start_date"), "%Y-%m-%d")
+            end_date = start_date + timedelta(int(request.POST.get("num_days")))
+            booking = Booking.objects.create(
+                guide="haval",
+                guest_name=request.POST.get("name"),
+                guest_email=request.POST.get("email"),
+                guest_phone=request.POST.get("phone_num"),
+                guest_country=request.POST.get("country"),
+                total_guests=request.POST.get("num_guests"),
+                start_date=start_date,
+                end_date=end_date,
+                currency="usd",
+                total_price=0,
+                amount_paid=0,
+                status="new request",
+                notes=request.POST.get("message"),
+            )
+            booking.itinerary.set(destinations)
+            booking_id = booking.id
+
+            # get country name from country code
+            country_code = request.POST.get("country")
+            country_name = Country(code=country_code).name
+
+            # form data collection for email
             form_context = {
+                "id": booking_id,
+                "status": "New Request",
                 "name": request.POST.get("name"),
                 "email": request.POST.get("email"),
                 "phone_num": request.POST.get("phone_num"),
-                "start_date": datetime.datetime.strftime(datetime.datetime.strptime(request.POST.get("start_date"), "%Y-%m-%d"), "%d %B, %Y"),  # noqa
-                "end_date": datetime.datetime.strftime(datetime.datetime.strptime(request.POST.get("start_date"), "%Y-%m-%d") + datetime.timedelta(int(request.POST.get("num_days"))), "%d %B, %Y"),  # noqa
+                "country": country_name,
+                "start_date": datetime.strptime(request.POST.get("start_date"), "%Y-%m-%d").strftime("%d %B, %Y"),  # noqa
+                "end_date": (datetime.strptime(request.POST.get("start_date"), "%Y-%m-%d") + timedelta(int(request.POST.get("num_days")))).strftime("%d %B, %Y"),  # noqa
                 "num_days": request.POST.get("num_days"),
+                "yyyy_mm": start_date.strftime("%Y-%m"),
                 "num_guests": request.POST.get("num_guests"),
                 "message": request.POST.get("message"),
-                "destinations": ", ".join(destinations),
+                "destinations": ", ".join([destination.name for destination in destinations]),  # noqa
             }
 
             # send email
             send_mail(
-                "New Trip Request (Iraqi Kurdistan Guide)",
+                "New Website Trip Request (Iraqi Kurdistan Guide)",
                 render_to_string(
                     "contact/emails/trip_request.txt",
                     {"form_context": form_context}
                 ),
                 settings.DEFAULT_FROM_EMAIL,
-                # [settings.DEFAULT_OWNER_EMAIL],  # TODO: link to Haval
-                [settings.DEFAULT_FROM_EMAIL],  # TODO: remove this
+                [settings.DEFAULT_OWNER_EMAIL],
             )
 
             return redirect(reverse("home"))
