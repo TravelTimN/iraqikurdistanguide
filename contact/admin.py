bd32acd07c0885c6ee3e6c01f776cd462ae6d70c
Tim Nelson <traveltimn@gmail.com>
2024-05-16 20:06:13 +0200

    Include new country field for Contact model

--- contact/admin.py
+++ contact/admin.py
@@ -6,7 +6,7 @@ from .models import Contact
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ("msg_date", "name", "email", "start_date", "num_days")
+    list_display = ("msg_date", "name", "email", "country", "start_date", "num_days")
     list_filter = ("email", )
     search_fields = ["email", "name"]
 
