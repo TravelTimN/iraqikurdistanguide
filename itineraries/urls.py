8b23a02773694cc14b6991512947f03b107b66f8
Tim Nelson <traveltimn@gmail.com>
2024-06-02 21:26:25 +0200

    Add CRUD URLs for Itin/ItinDays

--- itineraries/urls.py
+++ itineraries/urls.py
@@ -3,7 +3,12 @@ from . import views
 
 urlpatterns = [
     path("", views.itineraries, name="itineraries"),
-    # path("add/", views.add_faq, name="add_faq"),
-    # path("update/<int:id>", views.update_faq, name="update_faq"),
-    # path("delete/<int:id>", views.delete_faq, name="delete_faq"),
+    # itinerary
+    path("add/", views.add_itinerary, name="add_itinerary"),
+    path("update/<int:id>/", views.update_itinerary, name="update_itinerary"),  # noqa
+    path("delete/<int:id>/", views.delete_itinerary, name="delete_itinerary"),
+    # itinerary days
+    path("add_day/<int:id>/", views.add_itinerary_day, name="add_itinerary_day"),  # noqa
+    path("update_day/<int:itin_id>/<int:day_id>/", views.update_itinerary_day, name="update_itinerary_day"),  # noqa
+    path("delete_day/<int:itin_id>/<int:day_id>/", views.delete_itinerary_day, name="delete_itinerary_day"),  # noqa
 ]
