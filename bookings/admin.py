0f99d8a71e8d20e80e08e31b89c8083f20e28231
Tim Nelson <traveltimn@gmail.com>
2024-09-11 14:15:46 +0200

    Add date_created column to booking admin

--- bookings/admin.py
+++ bookings/admin.py
@@ -6,6 +6,6 @@ from .models import Booking
 class BookingAdmin(admin.ModelAdmin):
     list_display = (
         "guest_name", "start_date", "end_date",
-        "total_price", "amount_paid", "guide")
+        "total_price", "amount_paid", "guide", "date_created")
     list_filter = ("guide", )
     search_fields = ["guest_name", "guest_email"]
